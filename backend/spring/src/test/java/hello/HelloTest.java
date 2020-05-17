08836805772afbe5d07a0f99a4579d88690cfb87
Eduardo Demeneck Onghero <acrackintheice@gmail.com>
2020-05-16 23:24:09 -0300

    cleans unused files

--- backend/spring/src/test/java/hello/HelloTest.java
+++ backend/spring/src/test/java/hello/HelloTest.java
@@ -1,4 +1,17 @@
 package hello;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
 public class HelloTest {
+
+    @Test
+    public void testSomething() {
+        assertEquals(true, true);
+    }
+
 }
